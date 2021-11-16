803f983e6427d2066a2b96957ed6cb22af26702b
nahtlinh <94429889+nahtlinh@users.noreply.github.com>
2021-11-16 15:29:38 +0700

    Initialize patch-1

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Boys Day",
+    introTitle: 'To all the boys...of C6',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: 'Hi~',
+    title: 'Chúc các bạn 19/11 ....., Đọc được mấy dòng này vui không ạ?',
+    desc: '20-10 năm sau nhớ tổ chức rộn rã hơn năm nay nhá :))',
+    btnYes: 'Đương nhiên là vui rồi. Đọc cái này zuii quá mà',
+    btnNo: 'Không nha, không vui đâu ',
+    question: '10A6 ngoài .. bạn nữ (thêm vài tính từ miêu tả vào) thì còn gì nữa vậy?',
+    btnReply: 'Gửi đi với triệu trái tim',
+    reply: 'Còn có ... ... nữa nhá',
+    mess: 'Mình biết mà 🥰. Yêu các bạn nhiều nhiều 😘😘',
+}
